80c847edb90d2446e08befa67b5fb9150b788831
shgang <shgang97@163.com>
2025-08-10 02:21:36 +0800

    创建广告投放服务

---
+++ ads-service/ads-sponsor/src/main/java/cn/shgang/ads/sponsor/SponsorApplication.java
@@ -0,0 +1,11 @@
+package cn.shgang.ads.sponsor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class, args);
+    }
+}
